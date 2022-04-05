5405ffecd43b48f360e0d119973f2b0bf5bd3048
JORNAT <jornat.jerome@gmail.com>
2022-04-05 12:25:45 -0400

    Installation du server Node - backend
    Création et configuration des fichier server.js et app.js

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+module.exports = app;
