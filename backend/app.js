95041f1a21107c6fd21b983322dd29414e4e44d3
JORNAT <jornat.jerome@gmail.com>
2022-04-06 14:56:13 -0400

    Connexion avec Mysql

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,25 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require('express')
+const mysql = require('mysql2')
 const cors = require('cors')
-const path = require('path')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+
+const db = mysql.createConnection({
+  host : process.env.DB_HOST,
+  user : process.env.DB_USER,
+  password : process.env.DB_PASSWORD
+})
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connexion établi')
+})
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
