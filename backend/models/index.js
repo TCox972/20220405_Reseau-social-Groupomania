557ae47756ec89c9db164142992d8e84996770ad
JORNAT <jornat.jerome@gmail.com>
2022-05-29 10:25:37 -0400

    Variables d'env

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const dotenv = require('dotenv')
+dotenv.config()
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
@@ -12,7 +14,7 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, config);
 }
 
 fs
